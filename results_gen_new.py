dc0233582567af360c0ec988493e7b0ae88d0bcb
jessiiimai <maierj43@gmail.com>
2024-02-22 02:06:19 +0100

    newest update

--- results_gen_new.py
+++ results_gen_new.py
@@ -50,7 +50,7 @@ def generate_season(year, season):
         else:
             st.write(f"Link to  trailer: {url}")
     else:
-        st.write("With your settings, there was no title available. Please choose other settings!")
+        st.write("Oops! Something went wrong there! Please generate again or choose other settings.")
 
 
 def generate_genre(*genre_choice):
@@ -72,10 +72,8 @@ def generate_genre(*genre_choice):
     img = sex3["data"][num]["images"]["webp"]["image_url"]
     url = sex3["data"][num]["trailer"]["url"]
 
-    current_anime = [title]
-    st.session_state.current_anime = str(current_anime)
-    current_anime_img = [img]
-    st.session_state.img = str(current_anime_img)
+    st.session_state.current_anime = title
+    st.session_state.img = img
 
     st.write(title)
     st.image(img)
--- weebnote_login.py
+++ weebnote_login.py
@@ -15,6 +15,7 @@ db = connect_to_deta(base_name)
 # create the login form
 def login_form():
     with st.form("Login"):
+        st.image("images/headerwithtitle.png", use_column_width=True, width=10)
         st.markdown("Welcome! Please enter your login info.")
         username = st.text_input("Username", placeholder="Please enter your user name").lower()
         password = st.text_input("Password", placeholder="Please enter your password", type="password")
@@ -40,3 +41,12 @@ if st.session_state.logged_in:
     open_app()
 else:
     login_form()
+    # show some info text
+    st.markdown('<p style="font-weight: bold; font-size: 15px;'
+                '">3 reasons why you should get a Weebnote account now:</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 15px;">1. When you are feeling empty after finishing another great show, '
+                'you have a place to turn to.</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 15px;">2. You can bond with friends through our "matching" system.</p>',
+                unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 15px;">3. We are not only an ANIME GENERATOR, we are a community. </p>',
+                unsafe_allow_html=True)
